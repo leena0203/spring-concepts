f027f1e0c3b11bdbfb7467748a25fd3c1b5df2c6
leena0203 <leenasarode02@gmail.com>
2020-12-10 02:07:58 +0530

    Added: Logger to log messages

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -1,5 +1,7 @@
 package com.demo.springconcepts;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -8,12 +10,12 @@ import com.demo.springconcepts.component.DemoBean;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
-
+	public static final Logger logger = LoggerFactory.getLogger(SpringConceptsApplication.class);
 	public static void main(String[] args) {
-		System.out.println("Welcome to Spring Concepts");
+		logger.debug("Welcome to Spring Concepts");
 		ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
 		DemoBean demobean = context.getBean(DemoBean.class);
-		System.out.println("Demo Bean: "+demobean.toString() );
+		logger.debug("Demo Bean= "+demobean.toString() );
 	}
 
 }
